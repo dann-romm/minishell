0cf9edfab69d94d9b1f5968d64abb7cf02d87a69
dann-romm <danilka.2002.lol@gmail.com>
2022-02-24 15:58:58 +0300

    ft_strcmp added

--- src/utils_ft.c
+++ src/utils_ft.c
@@ -84,3 +84,13 @@ char	*ft_strdup(char *str)
 	p[i] = '\0';
 	return (p);
 }
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
