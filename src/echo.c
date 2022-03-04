740e3c4ccaef71504a1bd4f6a89239d903dacd1c
robbiekes <zaira.azhen@gmail.com>
2022-03-04 16:17:22 +0300

    echo finished

--- src/echo.c
+++ src/echo.c
@@ -1,22 +1,28 @@
 #include "shell.h"
 
-void	ft_echo(char **args)
+void	ft_echo(char **args, int arg_num)
 {
 	int	flag;
 	int	i;
 
 	flag = 0;
-	i = 0;
-
-	if (!args[0])
+	i = -1;
+	if (!args)
 	{
 		write(1, "\n", 1);
 		return ;
 	}
-	else if (args[0][0] == '-' && args[0][1] == 'n' && args[0][2] == '\n')
+	else if (args[0][0] == '-' && args[0][1] == 'n' && args[0][2] == '\0')
 	{
 		flag = 1;
-		i = 1;
+		i = 0;
 	}
-	while (++i)
+	while (args[++i])
+	{
+		ft_putstr(args[i]);
+		if (i < arg_num - 1)
+			write(1, " ", 1);
+	}
+	if (!flag)
+		write(1, "\n", 1);
 }
