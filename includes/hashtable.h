c253f6193c6f25c957aa07a7071cd1383ed4eb4d
Myrtice Gwynesse <mgwyness@student.21-school.ru>
2022-03-07 21:01:40 +0300

    global variable in .h

--- includes/hashtable.h
+++ includes/hashtable.h
@@ -7,6 +7,13 @@
 # include <errno.h>
 # include <string.h> // remove and write own memset()
 
+typedef struct s_shell
+{
+	t_hashtable	*env;
+}	t_shell;
+
+t_shell	*g_shell;
+
 typedef struct s_pair
 {
 	char			*key;
