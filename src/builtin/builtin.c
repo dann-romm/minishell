a7ca95dc81c6d8196aabe3c5179aadb5940b54bd
robbiekes <zaira.azhen@gmail.com>
2022-03-13 21:19:00 +0300

    setting pipe

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -3,25 +3,51 @@
 #include "libft_funcs.h"
 #include "builtin.h"
 
-void	exec_builtin(t_command_table *table, t_hashtable *ht)
+void	exec_cmd(t_command_table *table, t_simple_cmd *command)
+{
+
+}
+
+void	set_fork()
+{
+
+}
+
+void	check_cmd(t_command_table *table, t_simple_cmd *command)
 {
 	int	i;
 	i = -1;
 	while (++i < table->commands_num)
 	{
-		if (!ft_strcmp(table->commands[i]->cmd, "cd"))
-			ft_cd(ht, table->commands[i]);
-		if (!ft_strcmp(table->commands[i]->cmd, "pwd"))
-			ft_pwd(ht);
-		if (!ft_strcmp(table->commands[i]->cmd, "echo"))
+		if (command->type == CMD_CD)
+			ft_cd(g_shell->env_global, table->commands[i]);
+		if (command->type == CMD_PWD)
+			ft_pwd(g_shell->env_global);
+		if (command->type == 2)
 			ft_echo(table->commands[i]->cmd_args, table->commands[i]->args_num);
-		if (!ft_strcmp(table->commands[i]->cmd, "env"))
-			ft_env(ht);
-		if (!ft_strcmp(table->commands[i]->cmd, "export"))
+		if (command->type == 6)
+			ft_env(g_shell->env_global);
+		if (command->type == 3)
 			ft_export(table->commands[i]);
-		if (!ft_strcmp(table->commands[i]->cmd, "unset"))
-			ft_unset(ht, table->commands[i]);
-		if (!ft_strcmp(table->commands[i]->cmd, "exit"))
-			ft_exit(table->commands[i]->cmd_args, table->commands[i]->args_num);;
+		if (command->type == 5)
+			ft_unset(g_shell->env_global, table->commands[i]);
+		if (command->type == 4)
+			ft_exit(table->commands[i]->cmd_args, table->commands[i]->args_num);
+		else
+			exec_cmd(table, command);
+	}
+}
+
+void	execute(t_command_table *table)
+{
+	int i = 0;
+	if (table->commands_num == 1)
+		check_cmd(table, table->commands[0]);
+	else
+	{
+		while (i < table->commands_num)
+		{
+			pipe()
+		}
 	}
 }
