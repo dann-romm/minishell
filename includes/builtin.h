315c1b00f18404f45791dafc910dfd84f782d371
dann-romm <danilka.2002.lol@gmail.com>
2022-04-20 22:19:55 +0300

    refactoring

--- includes/builtin.h
+++ includes/builtin.h
@@ -3,10 +3,10 @@
 
 int		ft_cd(t_simple_cmd *cd);
 int		ft_echo(t_simple_cmd *command);
-int		ft_env();
+int		ft_env(void);
 int		ft_exit(char **cmd_args, int args_num);
 int		ft_export(t_simple_cmd *cd);
-int		ft_pwd();
+int		ft_pwd(void);
 int		ft_unset(t_simple_cmd *unset_cmd);
 int		ft_assignment(t_simple_cmd *cmd);
 
