ce52dcb8f08ba920a8f4e5d32e99fc643df68116
robbiekes <zaira.azhen@gmail.com>
2022-04-20 21:43:19 +0300

    unset.c fixed

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: mgwyness <mgwyness@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 21:16:34 by mgwyness          #+#    #+#             */
-/*   Updated: 2022/04/20 21:16:35 by mgwyness         ###   ########.fr       */
+/*   Updated: 2022/04/20 21:42:55 by mgwyness         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 #include "hashtable.h"
 #include "libft_funcs.h"
 
-int	ft_unset(t_simple_cmd *unset_cmd)
+int	ft_unset(t_simple_cmd *cmd)
 {
 	int		i;
 
-	i = -1;
-	if (!unset_cmd->args_num)
+	i = 0;
+	if (!cmd->args_num)
 		return (0);
-	else if (check_input(unset_cmd->cmd_args[0]))
+	while (i < cmd->args_num)
 	{
-		printf("minishell: unset: `%s': not a valid identifier\n", \
-			unset_cmd->cmd_args[0]);
-		errno = 1;
-		return (errno);
-	}
-	else
-	{
-		while (++i < unset_cmd->args_num)
+		if (check_input(cmd->cmd_args[i]))
 		{
-			if (find_hashtable(g_shell->env_global, unset_cmd->cmd) != 0)
-				remove_hashtable(&(g_shell->env_global), unset_cmd->cmd);
+			printf("minishell: unset: `%s': not a valid identifier\n", \
+				cmd->cmd_args[i]);
+			errno = 1;
 		}
+		else
+		{
+			if (find_hashtable(g_shell->env_global, cmd->cmd_args[i]) != 0)
+				remove_hashtable(g_shell->env_global, cmd->cmd_args[i]);
+		}
+		i++;
 	}
-	return (0);
+	return (errno);
 }
